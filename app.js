063f43947c8dbc12cb6a5757f1442a91f9170b8c
Brent Jameson <brentjameson@Brents-MBP.attlocal.net>
2021-11-17 11:25:05 -0500

    completed exercise

---
+++ app.js
@@ -0,0 +1,69 @@
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function(num) {
+//       return num % 2 === 0
+//     });
+//   }
+  
+  const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
+
+  const findMin = (...nums) => nums.reduce((min, currentVal) => min < currentVal ? min : currentVal)
+
+  const newObj1 = {a:1, b:2}
+  const newObj2 = {c:3, d:4}
+
+  const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
+
+//   const doubleAndReturnArgs = (arr, ...nums) => {
+//     return = nums.forEach(val) => val * 2
+//     return [...arr, ...numsDoubled]
+//   } 
+
+const doubleAndReturnArgs = (arr, ...nums) => [...arr, ...nums.map(val => val * 2)]
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+const removeRandom = (items) => {
+    let idx = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx+1)]
+}
+
+/** Return a new array with every item in array1 and array2. */
+
+const extend = (array1, array2) => [...array1, ...array2];
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+const addKeyVal = (obj, key, val) => {
+    let newObj = {...obj}
+    newObj[key] = val
+    return newObj
+}
+
+
+/** Return a new object with a key removed. */
+
+const removeKey = (obj, key) => {
+    let newObj = {...obj}
+    delete newObj[key]
+    return newObj
+}
+
+
+/** Combine two objects and return a new object. */
+
+const combine = (obj1, obj2) => ({...obj1, ...obj2})
+
+
+/** Return a new object with a modified key and value. */
+
+const update = (obj, key, val) => {
+    let newObj = {...obj}
+    newObj[key] = val
+    return newObj
+}
+
+  
+
